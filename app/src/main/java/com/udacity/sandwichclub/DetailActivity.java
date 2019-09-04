c48062f4d446da04d54e4dd0845199f46ef81a26
just-hussain <1just.hussain@gmail.com>
2019-09-05 00:12:49 +0300

    finalizing

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,26 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView tvAlsoKnownAs;
+    private TextView tvIngredients;
+    private TextView tvOrigin;
+    private TextView tvDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,11 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        tvAlsoKnownAs = findViewById(R.id.also_known_tv);
+        tvIngredients = findViewById(R.id.ingredients_tv);
+        tvOrigin = findViewById(R.id.origin_tv);
+        tvDescription = findViewById(R.id.description_tv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +57,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +70,28 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    //Checks and handles if there is an empty array or string.
+    private void populateUI(Sandwich sandwich) {
+        if (sandwich.getAlsoKnownAs().isEmpty())
+            tvAlsoKnownAs.setText("-none-");
+        else
+            tvAlsoKnownAs.setText(listToString(sandwich.getAlsoKnownAs()));
 
+        if (sandwich.getPlaceOfOrigin().equals(""))
+            tvOrigin.setText("-none-");
+        else
+            tvOrigin.setText(sandwich.getPlaceOfOrigin());
+        tvIngredients.setText(listToString(sandwich.getIngredients()));
+
+        tvDescription.setText(sandwich.getDescription());
+    }
+
+    //Creates a better looking bulleted string.
+    private static String listToString(List<String> list) {
+        String str = "";
+        for (int i = 0; i < list.size(); i++) {
+            str += "-" + list.get(i) + "\n";
+        }
+        return str;
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,45 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich sandwich = new Sandwich();
+
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+            JSONObject jsonName = jsonSandwich.getJSONObject("name");
+
+            sandwich.setMainName(jsonName.getString("mainName"));
+            sandwich.setAlsoKnownAs(listHelper(jsonName.getJSONArray("alsoKnownAs")));
+            sandwich.setPlaceOfOrigin(jsonSandwich.getString("placeOfOrigin"));
+            sandwich.setDescription(jsonSandwich.getString("description"));
+            sandwich.setImage(jsonSandwich.getString("image"));
+            sandwich.setIngredients(listHelper(jsonSandwich.getJSONArray("ingredients")));
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
+    }
+
+    // This method helps at converting a JSONArray to an ArrayList for easier manipulation.
+    private static List<String> listHelper(JSONArray arr) throws JSONException {
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length(); i++) {
+            list.add((String) arr.get(i));
+        }
+
+        return list;
     }
 }
